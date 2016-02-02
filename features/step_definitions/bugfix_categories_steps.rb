02d69b8a79cb31b95f4fee0f88fcef6d88b233c4
Engineered Edge <engineerededge@gmail.com>
2016-02-02 18:35:07 +0000

    bug is fixed but still need tests for creating and editing categories

--- features/step_definitions/bugfix_categories_steps.rb
+++ features/step_definitions/bugfix_categories_steps.rb
@@ -1,5 +1,8 @@
 Then /^the form fields should be blank$/ do
-  @category.name.should.be empty?
-  @category.keywords.should eq ''
-  find_field('permalinkgit').value.should eq ''
+  # @category.name.should.be empty?
+  # @category.keywords.should eq ''
+  find_field('category_name').value.should be_blank
+  find_field('category_permalink').value.should be_blank
+  find_field('category_keywords').value.should be_blank
+  find_field('category_description').value.should be_blank
 end
